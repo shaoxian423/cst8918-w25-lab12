6b56d0ee3f4f6cc27cc490561807d898f3e3deb4
RyanRen2023 <ren00055@algonquinlive.com>
2025-07-29 23:23:12 -0400

    fix ailed to query available provider packages

--- infra/tf-app/terraform.tf
+++ infra/tf-app/terraform.tf
@@ -1,10 +1,11 @@
 terraform {
-  required_version = ">= 1.3.0"
+
+  required_version = ">= 1.1.0"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.96.0"
+      version = "~> 3.0"
     }
   }
 
